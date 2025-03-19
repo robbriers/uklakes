e2c67c131992fc2d78155206cd22c1b6fec75787
Briers <40000865@napier.ac.uk>
2025-03-19 07:57:10 +0000

    fixing test!

--- tests/testthat/test-get_lake_info.R
+++ tests/testthat/test-get_lake_info.R
@@ -3,5 +3,5 @@ test_that("get_lake_info for a single lake returns a dataframe with one row", {
   # retrieve data for specific waterbody
   test_single_lake<-get_lake_info(24447)
   # check that it outputs a dataframe object
-  expect_true(nrow((test_single_lake)==1))
+  expect_equal(nrow((test_single_lake), 1))
 })
