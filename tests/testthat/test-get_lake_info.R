84abb30ab5b5de5947188d47c1cbd814b475a850
robbriers <r.briers@napier.ac.uk>
2025-03-21 20:56:01 +0000

    Update test-get_lake_info.R
    just fixing test description

--- tests/testthat/test-get_lake_info.R
+++ tests/testthat/test-get_lake_info.R
@@ -3,14 +3,14 @@
 test_that("get_lake_info for a single lake returns a dataframe with one row", {
   # retrieve data for specific waterbody
   test_single_lake<-get_lake_info(24447)
-  # check that it outputs a dataframe object
+  # check that it outputs a dataframe with one row
   expect_equal(nrow(test_single_lake), 1)
 })
 
 test_that("get_lake_info for a range of lakes returns a dataframe of right size", {
-  # retrieve data for specific waterbody
+  # retrieve data for a range of lakes
   test_range_lake<-get_lake_info(24444:24446)
-  # check that it outputs a dataframe object
+  # check that it outputs a dataframe of the expected size (3x16)
   expect_true(all(dim(test_range_lake)== c(3, 16)))
 })
 
@@ -20,6 +20,6 @@ test_that("invalid input returns an error", {
 })
 
 test_that("blank input returns a message", {
-  # retrieve data for lake wbid "Aardvark"
+  # try and retrieve info for blank input
   expect_error(get_lake_info())
 })
