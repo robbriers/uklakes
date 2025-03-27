bd277e5dd203749e6f06fe6fde309a6fab16ea39
Briers <40000865@napier.ac.uk>
2025-03-27 13:41:43 +0000

    typo in search_lakes docs

--- R/search_lakes.R
+++ R/search_lakes.R
@@ -7,7 +7,7 @@
 #
 #' @param string The search string to be matched (case-sensitive). Will match
 #' whole or partial strings in the column values.
-#'#'
+#'
 #' @return A data frame containing the details (Name & wbid) of all the sites
 #' that match the search string (full or partial matches).
 #'
